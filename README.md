5047e6c64880814e1c9bb0a39a3d198ff9f93675
almerindo <almerindo.rehem@gmail.com>
2021-06-30 13:00:45 -0300

    feat: add first structure

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# action-env-from-aws-ssm
-Creates a .env file from aws parameter store
+# action aws ssm to dotenv
+
+![](https://github.com/almerindo/action-env-from-aws-ssm/workflows/v1/badge.svg)
+
+create `.env` or **shell script** via AWS SSM parameters path
+
+## usage
+
+```yaml
+- uses: almerindo/action-env-from-aws-ssm
+  env:
+    AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }} # required
+    AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }} # required
+    AWS_DEFAULT_REGION: ap-northeast-2 # required
+  with:
+    ssm-path: /opensource/action-aws-ssm-to-dotenv # required
+    format: shell
+    output: .env.development
+    prefix: SSM_
+    decryption: true
+```
+
+⚠️ if output file already exists `action_aws_ssm_to_dotenv` will append data to output file(1.3.0)
+
+## option
+
+### ssm-path(required)
+
+AWS Systems Manager > Parameter Store > Path
+
+### format(default `dotenv`)
+
+optional, default=dotenv
+
+- dotenv: KEY="value" (default)
+- shell: export KEY="value"
+- yaml: KEY: "value"
+
+### output(default `.env`)
+
+output filename
+
+### prefix(optional)
+
+add prefix to exported variable name  
+eg) `prefix: ACTION_` will export `ACTION_ENV_VAR="value"`
+
+---
+
+### License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import * as core from '@actions/core'
+import * as AWS from 'aws-sdk'
+import {formatter} from './format'
+import {appendFileSync, existsSync, writeFileSync} from 'fs'
+import {GetParametersByPathResult, Parameter} from 'aws-sdk/clients/ssm'
+
+async function run() {
+  const region = process.env.AWS_DEFAULT_REGION
+  const ssm = new AWS.SSM({region})
+
+  try {
+    const ssmPath = core.getInput('ssm-path', {required: true})
+    const format = core.getInput('format', {required: true})
+    const output = core.getInput('output', {required: true})
+    const prefix = core.getInput('prefix')
+    const allParameters: Parameter[] = []
+    const withDecryption = core.getInput('decryption') === 'true'
+    let nextToken: string
+
+    try {
+      do {
+        const result: GetParametersByPathResult = await ssm
+          .getParametersByPath({
+            WithDecryption: withDecryption,
+            Path          : ssmPath,
+            Recursive     : true,
+            NextToken     : nextToken,
+          })
+          .promise()
+
+        core.debug(`parameters length: ${result.Parameters.length}`)
+        nextToken = result.NextToken
+        allParameters.push(...result.Parameters)
+      } while (nextToken)
+
+
+      const envs = allParameters
+        .map<Parameter>(p => ({
+          Value: p.Value,
+          Name : p.Name.split('/').pop(),
+        }))
+        .map<string>(formatter(format)(prefix))
+      if (envs.length > 0) {
+        envs.push('\n')
+      }
+
+      if (existsSync(output)) {
+        console.log(`append to ${output} file`)
+        appendFileSync(output, '\n' + envs.join('\n'))
+      } else {
+        console.log(`create ${output} file`)
+        writeFileSync(output, envs.join('\n'))
+      }
+    } catch (e) {
+      core.error(e)
+      core.setFailed(e.message)
+    }
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
+
+run()
