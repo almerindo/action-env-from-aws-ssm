9c1143adfd68e946d7f478b28f9b2c5cb4f801ea
almerindo <almerindo.rehem@gmail.com>
2022-05-03 21:48:25 -0300

    fix: configmap header

--- src/format/configmap.ts
+++ src/format/configmap.ts
@@ -7,7 +7,7 @@ export const yaml2ConfigMap = (content) =>{
     return "  " + element
   })
 
-  const header = "apiVersion: v1\r\nkind: ConfigMap\r\nmetadata:\r\n  name: ${KUBE_NAME}\r\n  namespace: ${KUBE_NAME}-env-cm\r\ndata:\r\n"
+  const header = "apiVersion: v1\r\nkind: ConfigMap\r\nmetadata:\r\n  name: ${KUBE_NAME}-env-cm\r\n  namespace: ${KUBE_NAME}\r\ndata:\r\n"
   const body = result.join('\n').toString();
   return header + body
 }
